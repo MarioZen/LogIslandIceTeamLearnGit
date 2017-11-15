afaf5c8ad11610c4227b1bbe975a0119573bb798
flucarelli <francesca.lucarelli@alliance-healthcare.net>
2017-11-15 12:29:17 +0100

    prima commit di fluka89

--- src/Lellospank/Lellospank.java
+++ src/Lellospank/Lellospank.java
@@ -6,6 +6,7 @@ public class Lellospank {
 	public static void main(String[] args) {
 System.out.println("Proprio lui");
 System.out.println("Grazie Mario by Carlo");
+System.out.println("Grazie Carlo by Fluka89");
 	}
 
 }
