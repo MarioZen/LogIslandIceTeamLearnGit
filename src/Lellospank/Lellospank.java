f9d7924e4b046354bf69541899d540ad48a9b328
mcerchia <maricerchia@gmail.com>
2017-11-20 17:00:18 +0100

    CoNFltTo

--- src/Lellospank/Lellospank.java
+++ src/Lellospank/Lellospank.java
@@ -9,7 +9,7 @@ public class Lellospank {
 		System.out.println("Proprio lui");
 		System.out.println("Grazie Mario by Carlo");
 		System.out.println(flukaInstance.flukaString("ciao belli!"));
-		System.out.println("Sergio organizza i meeting e poi se ne fugge");
+		System.out.println("fe   wufhiweghfuiwei ugge");
 	}
 
 }
